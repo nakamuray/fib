307eca4d91d4b596a41edbe510308a999953cf38
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2017-06-22 23:13:52 +0900

    for setuptools-rust>=0.6

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-from setuptools_rust import RustExtension
+from setuptools_rust import Binding, RustExtension
 
 
 setup(name='fib',
       version='0.1',
       rust_extensions=[
-          RustExtension('fib', 'extensions/Cargo.toml')],
+          RustExtension('fib', 'extensions/Cargo.toml',
+                        binding=Binding.RustCPython)],
       # rust extensions are not zip safe, just like C-extensions.
       zip_safe=False)
